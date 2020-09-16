9c2ee8465f96d6241c6be5912faf3ec27d14ddf3
Minhal Shanjer <mshanjer@gmail.com>
2020-09-15 23:31:07 -0400

    Added favicon

--- src/Login.js
+++ src/Login.js
@@ -5,10 +5,12 @@ import { loginUrl } from './spotify.js';
 function Login(){
     return (
         <div className="login">
+            <h4 className="signature">Mi Spotify</h4>
             <img src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
 
             {/* Login with spotify button */}
-            <a href={loginUrl}>LOGIN with SPOTIFY</a>
+            <a href={loginUrl}>Login via Spotify</a>
+            
         </div>
     )
 }
